face4c71c82a1c10c0a34b5530b763dadeb4e4c8
Bowie <rafaeldiassilv@gmail.com>
2025-05-20 00:04:37 -0300

    Feat: Revisando JS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Selheciona o documento html e pega o Elemento onde o id = "input-item"
 const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
 
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 //função de Retorno ou Função de Callback
 botaoAdicionar.addEventListener("click", (evento) => {
-    evento.preventDefault();
+    evento.preventDefault(); //previne o comportamento padrão de um formulario
 
     if (inputItem.value === "") {
         alert("Por favor, insira um item!");
@@ -14,7 +16,26 @@ botaoAdicionar.addEventListener("click", (evento) => {
 
     const itemDaLista = document.createElement("li");
     const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container")
+    containerItemDaLista.classList.add("lista-item-container");
 
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = "checkbox-" + contador++;
 
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckBox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+        weekday: "long",
+    });
+
+    const data = new Date().toLocaleDateString("pt-BR");
+    const dataCompleta = `${diaDaSemana} (${data})`;
+    console.log(dataCompleta);
 });
